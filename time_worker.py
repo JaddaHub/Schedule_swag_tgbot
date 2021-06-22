1240dfb535bde721df0dd4cbac6944725c480cf5
JaddaHub <maxim.y.mischenko@gmail.com>
2021-06-22 13:38:58 +0300

    time_worker commit

--- time_worker.py
+++ time_worker.py
@@ -1,43 +1,58 @@
-from datetime import datetime
 import json
+from datetime import datetime
+from datetime import timedelta
+
+
+# cur_time
+# отряд
+
+# расписание на день
+# what_next
+# оставшееся время
+# что сейчас
 
 
 class Shedule:
-    def __init__(self, datetime):
-        self.datetime = datetime
-
-        with open('timetable.json', 'r') as json_file:
+    def __init__(self, cur_datetime, squad, json_file):
+        self.cur_datetime = cur_datetime
+        self.squad = squad
+        self.shedule = json_file
+        if isinstance(json_file, json):
             self.shedule = json.load(json_file)
 
+        self.activity = self.what_activity()
 
     def what_now(self):
-        now = datetime.now()
-        try:
-            today = self.shedule[datetime.today()]
-            for hour in today.keys():
-                if hour <= now.hour:
-                    return today[hour]
-
-        except KeyError:
-            return 0
-
+        return self.activity[0]
 
     def what_next(self):
-        now = datetime.now()
+        return self.activity[1]
+
+    def what_activity(self):
         has_now = False
+        res = []
         try:
-            today = self.shedule[datetime.today()]
-            for hour in today.keys():
-                if hour <= now.hour and has_now:
-                    return today[hour]
+            today = self.shedule[self.cur_datetime.today()]
+            for hour in today[0].keys():
+                ref_hour = self._refact_time_to_datetime(hour)
+                if ref_hour <= self.cur_datetime and has_now:
+                    res.extend((hour, self.shedule[hour]))
                 elif not has_now:
                     has_now = True
-
+                    res.extend((hour, self.shedule[hour]))
         except KeyError:
-            return 0
+            return None
+        finally:
+            return res
 
+    def remaining_time(self):
+        t1 = self._refact_time_to_datetime(self.activity[1][0])
+        t2 = self._refact_time_to_datetime(self.activity[0][0])
+        return timedelta(t2 - t1)
 
-if __name__ == '__main__':
-    dt = datetime(1231, 11, 23, 21, 34, 45)
-    shed = Shedule(dt)
-    shed.what_now()
+    def show_shedule(self):
+        return self.shedule[self.cur_datetime.date][0]
+
+    def _refact_time_to_datetime(self, time_str):
+        return int(time_str[:time_str.find(':')]) * 60 + int(
+            time_str[time_str.find(':') + 1:])
