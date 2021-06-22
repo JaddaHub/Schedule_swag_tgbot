a7b9d0de4341bb388da629a9b257d464919f4207
JaddaHub <maxim.y.mischenko@gmail.com>
2021-06-22 15:22:03 +0300

    Merge remote-tracking branch 'origin/master' into Maxim
    # Conflicts:
    #	time_worker.py

--- time_worker.py
+++ time_worker.py
@@ -9,14 +9,13 @@ class Shedule:
         self.squad = squad
         self.JSON_NAME = 'json_timetable.json'
 
-        with open(self.JSON_NAME) as jsload:
+        with open(self.JSON_NAME, encoding='utf-8') as jsload:
             self.shedule = json.load(jsload)
 
         self.activity = self.what_activity()
 
     def what_now(self):
-        print(self.activity)
-        return self.activity[0]
+        return self.activity
 
     def what_next(self):
         return self.activity[1]
@@ -25,19 +24,8 @@ class Shedule:
         return tuple([i[0] for i in self.what_activity()])
 
     def what_activity(self):
-        has_now = False
-        res = []
-
-        today = self.shedule[self.squad][str(self.cur_datetime.day)]
-
-        for hour in today[0].keys():
-            ref_hour = self._refact_time_to_datetime(hour)
-            if ref_hour <= self.cur_datetime and has_now:
-                res.extend((hour, self.shedule[hour]))
-            elif not has_now:
-                has_now = True
-                res.extend((hour, today[0][hour]))
-        return res
+        for timing in self.shedule[self.squad][str(self.cur_datetime.day)]:
+            print(timing)
 
     def remaining_time(self):
         t1 = self._refact_time_to_datetime(self.activity[1][0])
@@ -45,7 +33,8 @@ class Shedule:
         return timedelta(t2 - t1)
 
     def show_shedule(self):
-        return self.shedule[self.cur_datetime.date][0]
+        return [timing for timing in
+                self.shedule[self.squad][str(self.cur_datetime.day)]]
 
     def _refact_time_to_datetime(self, time_str):
         time_str = time_str.split('-')[0]
@@ -55,7 +44,7 @@ class Shedule:
 
 
 if __name__ == '__main__':
-    print(datetime.now().day)
+    # print(datetime.now().day)
     sd = Shedule(datetime.now(), '1')
 
     print(sd.what_now())
