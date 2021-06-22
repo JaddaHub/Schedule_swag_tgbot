4f22097ba408bf5e50d2bc200ee65d4836656ee5
JaddaHub <maxim.y.mischenko@gmail.com>
2021-06-22 11:17:46 +0300

    commit

--- time_worker.py
+++ time_worker.py
@@ -1,22 +1,40 @@
 from datetime import datetime
-import csv
-
-
-def readcsv():
-    with open('template.csv', newline='') as csvfile:
-        reader = csv.reader(csvfile)
-    return reader
-
-
-SHEDULE = readcsv()
+import json
 
 
 class Shedule:
     def __init__(self, datetime):
         self.datetime = datetime
 
-    def what_now(self):
-        print(self.datetime)
+        with open('timetable.json', 'r') as json_file:
+            self.shedule = json.load(json_file)
+
+
+def what_now(self):
+    now = datetime.now()
+    try:
+        today = self.shedule[datetime.today()]
+        for hour in today.keys():
+            if hour <= now.hour:
+                return today[hour]
+
+    except KeyError:
+        return 0
+
+
+def what_next(self):
+    now = datetime.now()
+    has_now = False
+    try:
+        today = self.shedule[datetime.today()]
+        for hour in today.keys():
+            if hour <= now.hour and has_now:
+                return today[hour]
+            elif not has_now:
+                has_now = True
+
+    except KeyError:
+        return 0
 
 
 if __name__ == '__main__':
