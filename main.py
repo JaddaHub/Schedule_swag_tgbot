f52f1769453b68c33b232cab2195f50fae872a90
NickBogat <57542201+NickBogat@users.noreply.github.com>
2021-06-22 12:47:53 +0500

    кнопки

--- main.py
+++ main.py
@@ -9,13 +9,29 @@ dp = Dispatcher(bot)
 # Включаем логирование, чтобы не пропустить важные сообщения
 logging.basicConfig(level=logging.INFO)
 
+keyboard_general = types.ReplyKeyboardMarkup(resize_keyboard=True)
+keyboard_group = types.ReplyKeyboardMarkup(resize_keyboard=True)
+buttons_general = ["Выбрать отряд", "Общая информация"]
+buttons_group = ["5", "4", "3", "2", "1"]
+keyboard_general.add(*buttons_general)
+keyboard_group.add(*buttons_group)
 
-# Хэндлер на команду /test1
+
+@dp.message_handler(commands=['start'])
+async def process_start_command(message: types.Message):
+    await message.answer("Привет!", reply_markup=keyboard_general)
+
+
 @dp.message_handler(commands="test1")
 async def cmd_test1(message: types.Message):
     await message.reply("Test 1")
 
 
+@dp.message_handler(lambda message: message.text == "Выбрать отряд")
+async def registration(message: types.Message):
+    await message.answer("Выбери отряд с 1 по 5 номер",
+                         reply_markup=keyboard_group)
+
+
 if __name__ == "__main__":
-    # Запуск бота
     executor.start_polling(dp, skip_updates=True)
